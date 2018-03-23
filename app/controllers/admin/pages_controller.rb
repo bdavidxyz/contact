fade91d1b37c31b989c6a409f70d3179fd0f84e9
David Boureau <bdavidxyz@gmail.com>
2018-03-23 20:45:36 +0100

    wip

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -13,13 +13,34 @@ module Admin
       p csv_text.inspect
       p ''
 
-      csv = CSV.parse(csv_text, :headers => true)
+      csv = CSV.parse(csv_text, :headers => true, col_sep: ';')
       csv.each do |row|
         p '- - - - - - - - - - - - - - row- - - - - - - - - - - - - - - -' 
-        p row.inspect
+        p row.to_h.inspect
         p ''
-      end
 
+        first_name = row["First Name"]
+        last_name = row["Last Name"]
+        connected_on = Date.strptime(row["Connected On"].split(', ')[0].insert(-3, '20').gsub("/", "-"), '%m-%d-%Y')
+        position = row["Position"]
+        email = row["Email Address"]
+        company = row["Company"]
+
+        the_lead = Lead.new(
+            first_name: first_name,
+            last_name: last_name,
+            connected_on: connected_on,
+            position: position,
+            email: email,
+            company: company,
+        )
+      
+        p '- - - - - - - - - - - - - - the_lead- - - - - - - - - - - - - - - -' 
+        p the_lead.inspect
+        p ''
+        the_lead.save
+      end
+ 
 
     end
 
